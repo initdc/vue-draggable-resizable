b5f9f670e653f2c1230060967fa8d66543c5db0a
initdc <initd@outlook.com>
2021-04-05 18:14:17 +0800

    add logic for grid

--- stories/grid/grid-20-40.js
+++ stories/grid/grid-20-40.js
@@ -1,18 +1,56 @@
 import Vue from 'vue'
+let style = {
+  position: 'relative',
+  backgroundColor: '#808080',
+  background: 'linear-gradient(-90deg, rgba(0, 0, 0, .1) 1px, transparent 1px), linear-gradient(rgba(0, 0, 0, .1) 1px, transparent 1px)',
+  backgroundSize: '20px 40px, 20px 40px'
+}
 
-const style = {
+function setStyle(gridx, gridy) {
+  let bgSize = `${gridx}px ${gridy}px, ${gridx}px ${gridy}px`
+  // console.log(bgSize)
+  let sty = {
     position: 'relative',
     backgroundColor: '#808080',
     background: 'linear-gradient(-90deg, rgba(0, 0, 0, .1) 1px, transparent 1px), linear-gradient(rgba(0, 0, 0, .1) 1px, transparent 1px)',
-    backgroundSize: '20px 40px, 20px 40px'
+    backgroundSize: `${bgSize}`
+  }
+  // console.log(sty)
+  return sty
 }
 
 export default () => ({
+  data() {
+    let gridx = 0
+    let gridy = 0
+    let st = setStyle(gridx, gridy)
+    return {
+      gridx: gridx,
+      gridy: gridy,
+      st: st
+    }
+  },
+  methods: {
+    showGrid (newx, newy) {
+      let newst = setStyle(newx, newy)
+      this.$nextTick(function () {
+        this.gridx = newx
+        this.gridy = newy
+        this.st = newst
+      })
+
+      // return {
+      //   gridx: gridx,
+      //   gridy: gridy,
+      //   st: st
+      // }
+    }
+  },
   template: `
-    <div :style='${JSON.stringify(style)}'>
-      <vue-draggable-resizable :grid="[20,40]" :x="0" :y="0">
-        <p>Grid 20x40 starting from the top-left corner</p>
-      </vue-draggable-resizable>
+    <div :style="st">
+    <vue-draggable-resizable :grid="[gridx,gridy]" :x="0" :y="0" @dragging="showGrid(50,100)" @resizing="showGrid(50,100)" @deactivated="showGrid(0,0)">
+      <p>Grid 20x40 starting from the top-left corner</p>
+    </vue-draggable-resizable>
     </div>
   `
 })
